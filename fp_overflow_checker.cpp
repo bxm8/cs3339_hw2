de59af29866dccac7000675520d697cb063d5b9b
Tim <bxm8@txstate.edu>
2026-03-06 23:40:27 -0600

    Added argc and argv parameters to main
    And added comments outlining what I'm gonna do in the code

--- fp_overflow_checker.cpp
+++ fp_overflow_checker.cpp
@@ -5,6 +5,11 @@ using namespace std;
 float loop_counter;
 float loop_bound, increment;
 
-int main() {
+int main(int argc, char* argv[]) {
+    // Inputs
+    // Check for number of arguments
+
+    // Outputs
+    
     return 0;
 }
