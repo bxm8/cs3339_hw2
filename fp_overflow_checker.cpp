0ae8768bba3c97a8ac2817ddc4ef3ce1a1af61fb
Tim <bxm8@txstate.edu>
2026-03-06 22:27:41 -0600

    Moved variables outside main function

--- fp_overflow_checker.cpp
+++ fp_overflow_checker.cpp
@@ -2,11 +2,9 @@
 
 using namespace std;
 
+float loop_counter;
+float loop_bound, increment;
+
 int main() {
-    float loop_counter;
-
-    float loop_bound;
-    float increment;
-
     return 0;
 }
