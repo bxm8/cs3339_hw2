07ecc1a206126b9eeb4cb8e188169bf0d8efa435
Tim <bxm8@txstate.edu>
2026-03-07 00:00:17 -0600

    Added checks for too many or too few arguments
    Prints an error message and quits if number of arguments is wrong.

--- fp_overflow_checker.cpp
+++ fp_overflow_checker.cpp
@@ -7,7 +7,16 @@ float loop_bound, increment;
 
 int main(int argc, char* argv[]) {
     // Inputs
-    // Check for number of arguments
+    // Check for number of arguments and exit if wrong
+    if (argc != 3) {
+        cout << "usage:" << endl;
+        cout << "\t" << "./fp_overflow_checker loop_bound loop_counter" << endl;
+        cout << endl;
+        cout << "loop_bound is a positive floating-point value" << endl;
+        cout << "loop_counter is a positive floating-point value" << endl;
+        cout << endl;
+        return 1;
+    }
 
     // Outputs
     
