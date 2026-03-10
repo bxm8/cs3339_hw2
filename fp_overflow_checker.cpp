0d1e88e27a8bb72ad7dfd644767c6e1a855c12b8
Tim <bxm8@txstate.edu>
2026-03-09 22:40:07 -0500

    Removed unnecessary parameters for check_for_overflow function

--- fp_overflow_checker.cpp
+++ fp_overflow_checker.cpp
@@ -21,7 +21,7 @@ uint32_t counter_fraction;
 
 void convert_to_binary (float bound, float counter);
 void binary_output(uint32_t sign, uint32_t exponent, uint32_t fraction);
-bool check_for_overflow(float bound, float counter);
+bool check_for_overflow();
 
 int main(int argc, char* argv[]) {
     // Inputs
@@ -74,7 +74,7 @@ void binary_output(uint32_t sign, uint32_t exponent, uint32_t fraction) {
     cout << bitset<23>(fraction) << endl;
 }
 
-bool check_for_overflow(float bound, float counter) {
+bool check_for_overflow() {
     // Check to see if the bound's exponent is significantly more than the counter's exponent
     if (bound_exponent < counter_exponent + 24)
         return false;
